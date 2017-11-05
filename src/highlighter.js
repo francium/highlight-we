8a2cb5b1fa287f4497fd574f2223c2f8f9410611
francium <varunhooda1@gmail.com>
2017-11-05 01:53:12 -0400

    Remove commented out code
    - Note: The removed comments includes the experimental UI code.

--- src/highlighter.js
+++ src/highlighter.js
@@ -10,35 +10,13 @@ class App {
 
     this.ui = new UI(this.root);
 
-    // document.onselectionchange = (event) => {
-    //   document.body.style.border = "1px solid blue";
-    //   setTimeout(() => document.body.style.border = "1px solid red", 500);
-    //
-    //   const selection = document.getSelection();
-    //   if (selection) {
-    //     this.ui.draw(selection.focusNode.clientx, selection.focusNode.clienty);
-    //   } else {
-    //     this.ui.clearUI();
-    //   }
-    // };
-    //
     this.root.onmousedown = (event) => {
     };
 
-    // bind to some key (M) and only then do highlight
     document.onkeypress = (event) => {
-    // this.root.onmouseup = (event) => {
       if (event.key === "m") {
         thl.doHighlight();
-        // document.body.style.border = "1px solid red";
-        // const selection = window.getSelection();
-        //
-        // if (selection.toString()) {
-        //   let rects = (selection.getRangeAt(0).getClientRects());
-        //   let last = rects[rects.length - 1];
-        //   console.log(last);
-        //   this.ui.draw(last.x + last.width, last.y + last.height);
-        }
+      }
     };
   }
 }
